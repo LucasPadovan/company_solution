2e72ca9e5f354e59c8f7ac1a378d63c97711aca0
LucasPadovan <zerobba8@hotmail.com>
2017-05-12 02:00:48 -0300

    Added columns to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(
+    name: 'Admin',
+    lastname: 'Admin',
     email: 'admin@companymanager.com',
     password: '123456',
     password_confirmation: '123456'
