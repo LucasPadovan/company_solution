fde8004c962826f5a38c0d20a53430c5d6865771
LucasPadovan <zerobba8@hotmail.com>
2017-06-03 23:59:42 -0300

    Added trades back links

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -100,6 +100,7 @@ class TradesController < ApplicationController
     def new_form_url
       if params[:firm_id]
         @information[:form_url] = firm_trades_path(@parent, trade_type: params[:trade_type])
+        @information[:back_url] = firm_path(@parent)
         @information[:subtitle] = if params[:trade_type] == 'sells'
                                     t('view.trades.new_trade_sold_by_firm_title', firm: @parent.name)
                                   elsif params[:trade_type] == 'buys'
@@ -107,6 +108,7 @@ class TradesController < ApplicationController
                                   end
       elsif params[:product_id]
         @information[:form_url] = product_trades_path(@parent, trade_type: params[:trade_type])
+        @information[:back_url] = product_path(@parent)
         @information[:subtitle] = if params[:trade_type] == 'sells'
                                     t('view.trades.new_product_traded_by_title', product: @parent.name)
                                   elsif params[:trade_type] == 'buys'
@@ -119,8 +121,10 @@ class TradesController < ApplicationController
     def edit_form_url
       if params[:firm_id]
         @information[:form_url] = firm_trade_path(@parent, @trade)
+        @information[:back_url] = firm_path(@parent)
       elsif params[:product_id]
         @information[:form_url] = product_trade_path(@parent, @trade)
+        @information[:back_url] = product_path(@parent)
       end
     end
 
