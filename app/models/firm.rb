cb1011a55218349c69501a5f13d49565bbab127a
LucasPadovan <zerobba8@hotmail.com>
2017-07-30 17:46:13 -0300

    Strftime for prices dates

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -3,11 +3,11 @@ class Firm < ApplicationRecord
   has_many :orders
   has_many :trades
 
-  def open_time
+  def formatted_opens_at
     opens_at.strftime(I18n.t('time.formats.time')) if opens_at
   end
 
-  def close_time
+  def formatted_closes_at
     closes_at.strftime(I18n.t('time.formats.time')) if closes_at
   end
 
--- app/models/price.rb
+++ app/models/price.rb
@@ -2,4 +2,12 @@ class Price < ApplicationRecord
   belongs_to :trade
 
   scope :available, -> { where('available = ?', true) }
+
+  def formatted_valid_from
+    valid_from.strftime(I18n.t('date.formats.default')) if valid_from
+  end
+
+  def formatted_valid_to
+    valid_to.strftime(I18n.t('date.formats.default')) if valid_to
+  end
 end
