29e48561aa788c13a63a6efa0ad9cd47c558efae
LucasPadovan <zerobba8@hotmail.com>
2017-10-26 02:16:06 -0300

    Only the idea

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -4,7 +4,23 @@ class OrderLine < ApplicationRecord
 
   validates :product_id, presence: true
 
+
+  INCREASE_COLORS = {
+      first_break: 'green',
+      second_break: 'yellow',
+      third_break: 'orange',
+      fourth_break: 'red'
+  }
+
   def line_total
     subtotal + tax if subtotal && tax
+  end
+
+  def calculate_line_increase
+    # get order_date
+    # get last_product_price which valid_from < order_date and valid_to > order_date
+    # compare product > trade > available_price with last_product_price
+
+    #return one of the INCREASE_COLORS according to the amount of increase between last_product_price and available_price
   end
 end
